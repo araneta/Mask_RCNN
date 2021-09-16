6dadfbc726ac2b7421416fafaf0df90e74e831bc
Aldo praherda <aldo@aldoapp.com>
2021-09-16 13:36:38 +0000

    updated

--- mrcnn/visualize.py
+++ mrcnn/visualize.py
@@ -200,7 +200,7 @@ def display_instances(image, boxes, masks, class_ids, class_names,
                       scores=None, title="",
                       figsize=(16, 16), ax=None,
                       show_mask=True, show_bbox=True,
-                      colors=None, captions=None):
+                      colors=None, captions=None, outputfile=None):
     """
     boxes: [num_instance, (y1, x1, y2, x2, class_id)] in image coordinates.
     masks: [height, width, num_instances]
@@ -281,7 +281,8 @@ def display_instances(image, boxes, masks, class_ids, class_names,
             p = Polygon(verts, facecolor="none", edgecolor=color)
             ax.add_patch(p)
     ax.imshow(masked_image.astype(np.uint8))
-    plt.savefig('/content/Mask_RCNN/out.png',bbox_inches='tight', pad_inches=-0.5,orientation= 'landscape')
+    if outputfile:
+        plt.savefig(outputfile,bbox_inches='tight', pad_inches=-0.5,orientation= 'landscape')
     if auto_show:
         plt.show()
 
